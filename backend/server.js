34905088782a5ffa3da034cc8134330d5fa2bde7
Priyanka-1731 <89460533+Priyanka-1731@users.noreply.github.com>
2025-03-15 21:04:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -91,13 +91,5 @@ app.get("/api/profiles", async (req, res) => {
 
 // Server Listening
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 
-/*
-.env example
-MONGO_URI=mongodb+srv://<user>:<password>@cluster.mongodb.net/notes-app
-AWS_ACCESS_KEY_ID=YOUR_ACCESS_KEY
-AWS_SECRET_ACCESS_KEY=YOUR_SECRET_KEY
-AWS_REGION=ap-south-1
-S3_BUCKET_NAME=your-s3-bucket-name
-*/
